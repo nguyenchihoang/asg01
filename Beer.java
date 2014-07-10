26196af9defbf025ac62f57085f88e0ac539c782
Nguyen Chi Hoang <nguyenchihoang@Nguyens-MacBook-Pro.local>
2014-07-11 00:25:22 +0700

    Asigment 01

---
+++ Beer.java
@@ -0,0 +1,12 @@
+package asg01;
+
+public class Beer {
+    public static void main ( String [] args){
+	    System.out.println(" Ho va ten: Nguyen Chi Hoang \n Ma sinh vien: 12020159 \n Lop: K57CB \n username: chihoang \n Mail: nguyenchihoangapple@gmail.com \n\n");
+		
+		for(int i=9;i>0;i--){
+		    System.out.println(     i + " bottles of beer on the wall,  bottles of beer \n Take one down, pass in around");
+		}
+		System.out.println(" No more bottles of beer on the wall.");
+	}
+}
